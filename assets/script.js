c5e11de2d5b698f2f03f52ba3ee5d1ee8eaa0f70
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-05-03 15:47:31 +0200

    initialisation + html arrow + css dot + js init

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+//ecouteurs
+const arrowRight = document.querySelector(".arrow_right");
+const arrowLeft = document.querySelector(".arrow_left");
+
+arrowLeft.addEventListener("click", () => {
+console.log("click left")
+})
+
+arrowRight.addEventListener("click", () => {
+console.log("click right")
+})
+
+//dots
+const dots = document.querySelectorAll(".dot");
